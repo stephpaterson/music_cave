0224652fdfa0666dfb7f6322d93b3d35f3709305
Steph Paterson <steph.c.paterson@gmail.com>
2022-04-08 15:05:48 +0100

    tested saxophone can play

--- src/main/java/Instruments/Saxophone.java
+++ src/main/java/Instruments/Saxophone.java
@@ -1,11 +1,16 @@
 package Instruments;
 
-public class Saxophone extends Instrument{
+public class Saxophone extends Instrument implements IPlay{
 
     private SaxophoneType type;
 
     public Saxophone(String colour, String brand, SaxophoneType type) {
         super(colour, brand);
         this.type = type;
+    }
+
+    @Override
+    public String play() {
+        return "do do do dodododooooo";
     }
 }
---
+++ src/test/java/SaxophoneTest.java
@@ -0,0 +1,21 @@
+import Instruments.Saxophone;
+import Instruments.SaxophoneType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SaxophoneTest {
+
+    Saxophone saxophone;
+
+    @Before
+    public void before(){
+        saxophone = new Saxophone("gold", "yamaha", SaxophoneType.ALTO);
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("do do do dodododooooo", saxophone.play());
+    }
+}
